edfe95e3c8b35abed6841ece29904036524abf87
Manan <gouhari21@hotmail.com>
2020-10-02 23:27:39 +0530

    CSSBaseline added from material-ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import CSSBaseline from '@material-ui/core/CSSBaseline';
+
 
 function App() {
   return (
+
     <div className="App">
+      <CSSBaseline />
       <h1>MagicRead</h1>
     </div>
   );
