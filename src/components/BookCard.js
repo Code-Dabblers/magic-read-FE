683b1956c5a268a22367dda82b20de8330bbb67e
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-10-15 18:31:29 -0400

    Assing read buttong for book card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,4 +1,7 @@
-import { Card, CardMedia } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
@@ -7,10 +10,10 @@ import React from "react";
 
 const useStyles = makeStyles({
 	root: {
-		maxWidth: 210
+		maxWidth: 345
 	},
 	media: {
-		height: 170
+		height: 140
 	}
 });
 
@@ -23,14 +26,17 @@ function BookCard(props) {
 			</CardActionArea>
 			<CardContent>
 				<Typography gutterBottom variant="h5" component="h2">
-					{props.book_title}
+					{/* {props.book_title} */}
 					Title
 				</Typography>
 				<Typography variant="body2" color="textSecondary" component="p">
-					{props.author}
+					{/* {props.author} */}
 					Author
 				</Typography>
 			</CardContent>
+			<CardActions>
+				<Button size="medium" color="primary">Read</Button>
+			</CardActions>
 		</Card>
 	);
 }
