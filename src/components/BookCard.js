02ee28f5c1c7e4a9715038fb722f85b3ef60529e
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-10-15 22:34:37 -0400

    Adding Rating to the card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,19 +1,20 @@
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import CardContent from "@material-ui/core/CardContent";
-import React from "react";
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import Rating from '@material-ui/lab/Rating';
+import React from 'react';
 
 const useStyles = makeStyles({
 	root: {
 		maxWidth: 345
 	},
 	media: {
-		height: 140
+		height: 240
 	}
 });
 
@@ -22,19 +23,21 @@ function BookCard(props) {
 	return (
 		<Card className={classes.root}>
 			<CardActionArea>
-				<CardMedia className={classes.media} image="{props.book_title}" title="Book Thumbnail" />
+				<CardMedia className={classes.media} image="{props.thumbnail}" title="Book Thumbnail" />
+				<Rating name="read-only" value="{props.rating}" readOnly size="large" />
+				<CardContent className={classes.content}>
+					<Typography gutterBottom variant="h5" component="h5">
+						{props.book_title}
+					</Typography>
+					<Typography variant="subtitle1" color="textSecondary" component="p">
+						{props.author}
+					</Typography>
+				</CardContent>
 			</CardActionArea>
-			<CardContent>
-				<Typography gutterBottom variant="h5" component="h2">
-					{props.book_title}
-					Title
-				</Typography>
-				<Typography variant="body2" color="textSecondary" component="p">
-					{props.author}
-				</Typography>
-			</CardContent>
 			<CardActions>
-				<Button size="medium" color="primary">Read</Button>
+				<Button size="small" color="primary">
+					Read
+				</Button>
 			</CardActions>
 		</Card>
 	);
