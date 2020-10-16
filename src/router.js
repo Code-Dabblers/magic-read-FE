c3cfc05d1f839e58ffeed2be60b2e259e358a14b
mihirgupta0900 <mihirgupta0900@gmail.com>
2020-10-16 15:39:35 +0530

    updated readme and added .prettierrc

--- src/router.js
+++ src/router.js
@@ -1,38 +1,26 @@
-import React from 'react'
-import {
-    BrowserRouter,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 
 export default function Router() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route
-                        path="/"   
-                        exact                 
-                    >
-                        <Home />
-                    </Route>
-                    <Route
-                        path="/auth/login"
-                    >
-                        <Login />
-                    </Route>
-                    <Route
-                        path="/auth/register"
-                    >
-                        <Register />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    )
+	return (
+		<div>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+					<Route path="/auth/login">
+						<Login />
+					</Route>
+					<Route path="/auth/register">
+						<Register />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
