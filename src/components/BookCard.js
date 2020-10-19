71ed5ce20f9b1962d7ff98dcf4fbcb80bd9ea7c4
Manan <gouhari21@hotmail.com>
2020-10-19 12:04:29 +0530

    BookCard component refactored.

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -11,13 +11,13 @@ import React from 'react';
 
 const useStyles = makeStyles({
 	root: {
-		maxWidth: 345
+		maxWidth: "300px"
 	},
 	media: {
 		height: 240
 	},
 	rating: {
-		paddingLeft: 15
+		paddingBottom: "8px"
 	},
 	typography: {
 		paddingLeft: 5
@@ -26,25 +26,34 @@ const useStyles = makeStyles({
 
 export default function BookCard({ thumbnail, rating, book_title, author }) {
 	const classes = useStyles();
+	/* Example usage - 
+	<BookCard
+		book_title="Title for the book"
+		author="AuthAuth"
+		rating={3}
+		thumbnail="https://images.unsplash.com/photo-1603005674855-b9f6334b7f92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
+	/> */
 	return (
 		<Card className={classes.root}>
+			
+			<CardMedia className={classes.media} image={thumbnail} title="Book Thumbnail" />
+			<CardContent>
+				<Rating className={classes.rating} value={rating} readOnly size="large" />
+				<Typography className={classes.typography} variant="h5" component="h5">
+					{book_title}
+				</Typography>
+				<Typography className={classes.typography} variant="subtitle1" color="textSecondary" component="p">
+					{author}
+				</Typography>
+			</CardContent>
+	
 			<CardActionArea>
-				<CardMedia className={classes.media} image={thumbnail} title="Book Thumbnail" />
-				<Rating className={classes.rating} name="read-only" value={rating} readOnly size="large" />
-				<CardContent>
-					<Typography className={classes.typography} gutterBottom variant="h5" component="h5">
-						{book_title}
-					</Typography>
-					<Typography className={classes.typography} variant="subtitle1" color="textSecondary" component="p">
-						{author}
-					</Typography>
-				</CardContent>
+				<CardActions>
+					<Button size="small" color="primary">
+						Read
+					</Button>
+				</CardActions>
 			</CardActionArea>
-			<CardActions>
-				<Button size="small" color="primary">
-					Read
-				</Button>
-			</CardActions>
 		</Card>
 	);
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,12 +9,6 @@ export default function Home() {
   return (
     <Container>
       Home page/Landing page
-      <BookCard 
-        book_title="Title for the book"
-        author = "AuthAuth"
-        rating={3}
-        thumbnail= "https://images.unsplash.com/photo-1603005674855-b9f6334b7f92?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
-      />
     </Container>
   );
 }
